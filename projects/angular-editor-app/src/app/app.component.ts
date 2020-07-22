a571d81975d8e173351d9c2ae208e36390c759a4
Petr Humplík <petr.humplik@netdirect.cz>
2020-07-22 12:09:23 +0200

    - insert ftp icon - insert table icon - multi-lang support

--- projects/angular-editor-app/src/app/app.component.ts
+++ projects/angular-editor-app/src/app/app.component.ts
@@ -3,104 +3,108 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {AngularEditorConfig} from '../../../angular-editor/src/public-api';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+    title = 'app';
 
-  form: FormGroup;
+    form: FormGroup;
 
-  htmlContent1 = '';
-  htmlContent2 = '';
+    htmlContent1 = '';
+    htmlContent2 = '';
+    langCode: 'cs' | 'en' = 'cs';
 
-  config1: AngularEditorConfig = {
-    editable: true,
-    spellcheck: true,
-    minHeight: '10rem',
-    maxHeight: '25rem',
-    placeholder: 'Enter text here...',
-    translate: 'no',
-    sanitize: false,
-    toolbarPosition: 'top',
-    outline: false,
-    defaultFontName: 'Times New Roman',
-    defaultFontSize: '3',
-    showToolbar: true,
-    defaultParagraphSeparator: 'p',
-    customClasses: [
-      {
-        name: 'quote',
-        class: 'quote',
-      },
-      {
-        name: 'redText',
-        class: 'redText'
-      },
-      {
-        name: 'titleText',
-        class: 'titleText',
-        tag: 'h1',
-      },
-    ],
-    toolbarHiddenButtons: [
-      // ['bold', 'italic'],
-      // ['fontSize']
-    ]
-  };
+    config1: AngularEditorConfig = {
+        editable: true,
+        spellcheck: true,
+        minHeight: '10rem',
+        maxHeight: '25rem',
+        placeholder: 'Enter text here...',
+        translate: 'no',
+        sanitize: false,
+        toolbarPosition: 'top',
+        outline: false,
+        defaultFontName: 'Times New Roman',
+        defaultFontSize: '3',
+        showToolbar: true,
+        defaultParagraphSeparator: 'p',
+        language: this.langCode,
+        customClasses: [
+            {
+                name: 'quote',
+                class: 'quote',
+            },
+            {
+                name: 'redText',
+                class: 'redText'
+            },
+            {
+                name: 'titleText',
+                class: 'titleText',
+                tag: 'h1',
+            },
+        ],
+        toolbarHiddenButtons: [
+            // ['bold', 'italic'],
+            ['insertTable']
+        ]
+    };
 
-  config2: AngularEditorConfig = {
-    editable: true,
-    spellcheck: true,
-    minHeight: '5rem',
-    maxHeight: '15rem',
-    placeholder: 'Enter text here...',
-    translate: 'no',
-    sanitize: true,
-    toolbarPosition: 'bottom',
-    defaultFontName: 'Comic Sans MS',
-    defaultFontSize: '5',
-    defaultParagraphSeparator: 'p',
-    customClasses: [
-      {
-        name: 'quote',
-        class: 'quote',
-      },
-      {
-        name: 'redText',
-        class: 'redText'
-      },
-      {
-        name: 'titleText',
-        class: 'titleText',
-        tag: 'h1',
-      },
-    ],
-    toolbarHiddenButtons: [
-      ['bold', 'italic'],
-      ['fontSize']
-    ]
-  };
+    config2: AngularEditorConfig = {
+        editable: true,
+        spellcheck: true,
+        minHeight: '5rem',
+        maxHeight: '15rem',
+        placeholder: 'Enter text here...',
+        translate: 'no',
+        sanitize: true,
+        toolbarPosition: 'bottom',
+        defaultFontName: 'Comic Sans MS',
+        defaultFontSize: '5',
+        defaultParagraphSeparator: 'p',
+        language: this.langCode,
+        customClasses: [
+            {
+                name: 'quote',
+                class: 'quote',
+            },
+            {
+                name: 'redText',
+                class: 'redText'
+            },
+            {
+                name: 'titleText',
+                class: 'titleText',
+                tag: 'h1',
+            },
+        ],
+        toolbarHiddenButtons: [
+            ['bold', 'italic'],
+            ['fontSize']
+        ]
+    };
 
-  constructor(private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder) {
+    }
 
-  ngOnInit() {
-    this.form = this.formBuilder.group({
-      signature: ['', Validators.required]
-    });
-    console.log(this.htmlContent1);
-  }
+    ngOnInit() {
+        this.form = this.formBuilder.group({
+            signature: ['', Validators.required]
+        });
+        console.log(this.htmlContent1);
+    }
 
-  onChange(event) {
-    console.log('changed');
-  }
+    onChange(event) {
+        console.log('changed');
+    }
 
-  onBlur(event) {
-    console.log('blur ' + event);
-  }
+    onBlur(event) {
+        console.log('blur ' + event);
+    }
 
-  onChange2(event) {
-    console.warn(this.form.value);
-  }
+    onChange2(event) {
+        console.warn(this.form.value);
+    }
 }
---
+++ projects/angular-editor/src/lib/services/lang.service.ts
@@ -0,0 +1,103 @@
+import {EventEmitter, Injectable} from '@angular/core';
+
+const sen_en: { [p: string]: string } = {
+    undo: 'Undo',
+    redo: 'Redo',
+    bold: 'Bold',
+    italic: 'Italic',
+    underline: 'Underline',
+    strikethrough: 'Strikethrough',
+    subscript: 'Subscript',
+    superscript: 'Superscript',
+    justifyLeft: 'Justify Left',
+    justifyCenter: 'Justify Center',
+    justifyRight: 'Justify Right',
+    justifyFull: 'Justify Full',
+    indent: 'Indent',
+    outdent: 'Outdent',
+    unorderedList: 'Unordered List',
+    orderedList: 'Ordered List',
+    insertLink: 'Insert Link',
+    unlink: 'Unlink',
+    insertImage: 'Insert Image',
+    insertFtp: 'Insert File/Image from FTP',
+    insertVideo: 'Insert Video',
+    horizontalLine: 'Horizontal Line',
+    insertTable: 'Insert Table',
+    clearFormatting: 'Clear Formatting',
+    HTMLCode: 'HTML Code',
+    textColor: 'Text Color',
+    backgroundColor: 'Background Color'
+};
+
+const sen_cs: { [p: string]: string } = {
+    undo: 'Zpět',
+    redo: 'Znovu',
+    bold: 'Tučné',
+    italic: 'Kurzíva',
+    underline: 'Podtržené',
+    strikethrough: 'Přeškrtnuté',
+    subscript: 'Dolní index',
+    superscript: 'Horní index',
+    justifyLeft: 'Zarovnat vlevo',
+    justifyCenter: 'Zarovnat doprostřed',
+    justifyRight: 'Zarovnat vpravo',
+    justifyFull: 'Zarovnat do bloku',
+    indent: 'Odsazení',
+    outdent: 'Zmenšit odsazení',
+    unorderedList: 'Nesetříděný seznam',
+    orderedList: 'Setříděný seznam',
+    insertLink: 'Vložit odkaz',
+    unlink: 'Zrušit odkaz',
+    insertImage: 'Vložit obrázek',
+    insertFtp: 'Vložit soubor/obrázek z FTP',
+    insertVideo: 'Vložit video',
+    horizontalLine: 'Vodorovná čára',
+    insertTable: 'Vložit tabulku',
+    clearFormatting: 'Zrušit formátování',
+    HTMLCode: 'HTML kód',
+    textColor: 'Barva textu',
+    backgroundColor: 'Barva pozadí'
+};
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LangService {
+    set lang(value: 'cs' | 'en') {
+        this._lang = value;
+        this.switchLang(value);
+    }
+
+    private _lang: 'cs' | 'en' = 'cs';
+
+    get sen(): { [p: string]: string } {
+        return this._sen;
+    }
+
+    set sen(value: { [p: string]: string }) {
+        this._sen = value;
+    }
+
+    private _sen: { [key: string]: string } = {};
+
+    languageChanged: EventEmitter<{ [key: string]: string }> = new EventEmitter<{ [key: string]: string }>();
+
+    constructor() {
+        this.switchLang('cs');
+    }
+
+    private switchLang(val: 'cs' | 'en'): void {
+        switch (val) {
+            case 'cs':
+                this.sen = sen_cs;
+                break;
+            case 'en':
+                this.sen = sen_en;
+                break;
+            default:
+                this.sen = sen_cs;
+        }
+        this.languageChanged.emit(this.sen);
+    }
+}
